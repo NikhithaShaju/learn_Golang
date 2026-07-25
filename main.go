eb600ae2780b7afd6f150dc3e4dece404d63e20d
NikhithaShaju <nikhithashaju2003@gmail.com>
2026-07-25 22:54:37 +0530

    first go function

--- main.go
+++ main.go
@@ -4,5 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello")
+	printMyName("nikhitha", 22, 12)
 
+}
+func printMyName(name string, age int, class int) {
+	fmt.Println("name=",name)
+	fmt.Println("age=",age)
+	fmt.Println("class=",class)
 }
