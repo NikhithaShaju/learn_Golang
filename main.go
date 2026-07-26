ce36ff4ab29d775924ab4eaad3a0c191f94fbfdc
NikhithaShaju <nikhithashaju2003@gmail.com>
2026-07-26 13:06:27 +0530

    second go function

--- main.go
+++ main.go
@@ -1,14 +1,33 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("hello")
+// 	printMyName("nikhitha", 22, 12)
+
+// }
+//
+//	func printMyName(name string, age int, class int) {
+//		fmt.Println("name=",name)
+//		fmt.Println("age=",age)
+//		fmt.Println("class=",class)
+//	}
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("hello")
-	printMyName("nikhitha", 22, 12)
-
+	printStudentDetails("nikhitha", 22, 12)
 }
-func printMyName(name string, age int, class int) {
-	fmt.Println("name=",name)
-	fmt.Println("age=",age)
-	fmt.Println("class=",class)
+func printStudentDetails(name string, age int, class int) {
+	for i := 0; i < 5; i++ {
+		fmt.Println("name=", name)
+	}
+	for j := 0; j < 3; j++ {
+		fmt.Println("age=", age)
+	}
+	for k := 0; k < 4; k++ {
+		fmt.Println("class=", class)
+	}
 }
