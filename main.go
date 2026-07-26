aee99675a7b9b850b73087a8b6a1764e35fdf955
NikhithaShaju <nikhithashaju2003@gmail.com>
2026-07-26 21:28:51 +0530

    learned functions

--- main.go
+++ main.go
@@ -1,33 +1,73 @@
-// package main
-
-// import "fmt"
-
-// func main() {
-// 	fmt.Println("hello")
-// 	printMyName("nikhitha", 22, 12)
-
-// }
-//
-//	func printMyName(name string, age int, class int) {
-//		fmt.Println("name=",name)
-//		fmt.Println("age=",age)
-//		fmt.Println("class=",class)
-//	}
 package main
 
 import "fmt"
 
 func main() {
-	printStudentDetails("nikhitha", 22, 12)
+	// fmt.Println("hello")
+	// printMyName("nikhitha", 22, 12)
+	// printStudentDetails("nithya", 20, 11, 6, 4, 7)
+	// calculateAge(2004)
+	// calculateAge(2007)
+	// studentGrade("anu", 6)
+	// studentGrade("riya", 9)
+	// studentGrade("liya", 7)
+	// oddOrEven(5)
+	// oddOrEven(8)
+	lengthOfName("nithya")
+	lengthOfName("anav")
+
 }
-func printStudentDetails(name string, age int, class int) {
-	for i := 0; i < 5; i++ {
+
+func lengthOfName(name string) {
+	count := len(name)
+	fmt.Println(count)
+
+}
+
+func oddOrEven(num int) {
+	if num%2 == 0 {
+		fmt.Println(num, "is even")
+	} else {
+		fmt.Println(num, "is odd")
+
+	}
+
+}
+
+func studentGrade(name string, mark int) {
+	if mark >= 8 {
+		fmt.Println("A grade for", name)
+	}
+	if mark <= 7 && mark > 5 {
+		fmt.Println("B grade for", name)
+	}
+	if mark <= 5 {
+		fmt.Println("C grade for", name)
+	}
+
+}
+
+func calculateAge(birthYear int) {
+	currentYear := 2026
+	age := 0
+	age = currentYear - birthYear
+	fmt.Println("age=", age)
+}
+
+func printMyName(name string, age int, class int) {
+	fmt.Println("name=", name)
+	fmt.Println("age=", age)
+	fmt.Println("class=", class)
+}
+
+func printStudentDetails(name string, age int, class int, nameCount int, ageCount int, classCount int) {
+	for i := 0; i < nameCount; i++ {
 		fmt.Println("name=", name)
 	}
-	for j := 0; j < 3; j++ {
+	for j := 0; j < ageCount; j++ {
 		fmt.Println("age=", age)
 	}
-	for k := 0; k < 4; k++ {
+	for k := 0; k < classCount; k++ {
 		fmt.Println("class=", class)
 	}
 }
